aaae321932ffbd93e60a5921dfe85f689bf5995d
Devanshubist <devanshubisht66@gmail.com>
2025-04-22 12:22:34 +0530

    changes done

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 // Enable CORS for frontend access
-app.use(cors());
+app.use(cors());  
 
 app.post('/api/insertTRReport', async (req, res) => {
   try {
