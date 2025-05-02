9cd473390869ac98ee85f77c7ead9116334d4343
iamvanshkumar <vansh6543@gmail.com>
2025-05-02 14:39:37 +0530

    home page setup started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,6 @@ export default function Home() {
     }
   }, []);
 
-  
   const history = useNavigate();
 
   useEffect(() => {
@@ -24,9 +23,21 @@ export default function Home() {
   return (
     <>
       <SideNav activeTab="home" />
-      <main className="">
-
+      <main className="col-span-4 flex flex-col gap-2 h-full overflow-y-scroll p-2">
+        <section className="grid grid-cols-4 gap-2">
+          <div className="bg-blue-400 text-white p-3 flex flex-col items-center justify-center gap-0.5 rounded-md shadow-md border border-blue-300">
+            <h2 className="font-semibold">Total Number of Reports</h2>
+            <span className="font-bold text-2xl">100</span>
+            <span className="font-semibold text-sm">Insights Fetcher</span>
+          </div>
+          <div className="bg-red-400 text-white p-3 flex flex-col items-center justify-center gap-0.5 rounded-md shadow-md border border-red-300">
+            <h2 className="font-semibold">Total Number of Reports</h2>
+            <span className="font-bold text-2xl">100</span>
+            <span className="font-semibold text-sm">Counter Fetcher</span>
+          </div>
+        </section>
       </main>
+      
     </>
   );
 }
