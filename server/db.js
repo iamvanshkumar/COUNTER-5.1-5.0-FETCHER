9bd888cd39a9e75d51f140d3647a7db460f9e06d
Devanshubist <devanshubisht66@gmail.com>
2025-05-13 11:26:28 +0530

    working code

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'fetcher_user',
-  password: 'your_password',
+  user: 'root',
+  password: 'Devans@99978',
   database: 'fetcher_db',
   waitForConnections: true,
   connectionLimit: 10,
