4541e3d028d069dcaec3012ddf99a2cfbade2f18
iamvanshkumar <vanshkumar.mail@gmail.com>
2025-07-22 12:07:12 +0530

    removed extra elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import {Routes, Route, Navigate } from "react-router-dom";
 import FetcherReports from "./pages/FetcherReports";
-import Settings from "./pages/Settings";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<FetcherReports />} />
         <Route path="*" element={<Navigate to="/" />} />
-        <Route path="settings" element={<Settings/>} />
       </Routes>
   );
 }
